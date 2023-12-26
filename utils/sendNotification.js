2c8fad22323d47b1c32a7e88214e12c1ae189bc7
sudeepmi <sudeepm0599@gmail.com>
2023-12-26 19:01:20 +0545

    sort donors by distance

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -4,12 +4,13 @@ module.exports.sendNotification = async (userToken,message) => {
     axios.post("https://fcm.googleapis.com/fcm/send",
         {
             "registration_ids": userToken,
-            "collapse_key": "New Blood Request",
+            "collapse_key": "Open",
             "priority": "high",
             "notification": {
                 "title": "Blood Request",
                 "body": message
-            }
+            },
+            "sound":"alert.wav"
         },{
             headers:{
                 Authorization:"key=AAAAsHV4IqU:APA91bFx4b5z3UXuUd9lrsNNqC9YEl5J61FRkbqYbkXEOQmnrnYF25TX-kAbRyHGWa2UjZOg8NNRB0A3mZZF7yjfZeCWMVLhyJ60Wb_VV9aJoyiOLxBo_uiy2f2UmDxCeZfL8uovBIsi",
