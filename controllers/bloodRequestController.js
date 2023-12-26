2acaf983bf1b1f73468d2d261ca0daccef61e0a9
sudeepmi <sudeepm0599@gmail.com>
2023-12-26 18:36:54 +0545

    send notifications to potential donors

--- controllers/bloodRequestController.js
+++ controllers/bloodRequestController.js
@@ -2,11 +2,13 @@ const asyncHandler = require("express-async-handler");
 const BloodRequest = require("../models/request");
 const User = require("../models/user");
 const { calculateDistance } = require("../utils/algo");
+const { sendNotification } = require("../utils/sendNotification");
 
 const saveBloodRequest = asyncHandler(async (req, res) => {
-    const {gender,cordinates,location,blood_group} = req.body;
+    const {gender,cordinates,location,blood_group,emergency} = req.body;
     const step = req.query.step;
     const initiator = req.user._id;
+    const notification_tokens = [];
     if(step==2){
         const _donors = await User.find({'profile.blood_group':blood_group,'_id': { $ne: initiator }});
         if(_donors.length>0){
@@ -15,11 +17,13 @@ const saveBloodRequest = asyncHandler(async (req, res) => {
                 const { age, weight } = profile[0];
                 const distanceFromuserLocation = calculateDistance(cordinates, req.user.last_location);
                 const distanceFromPreferedLocation = calculateDistance(cordinates, obj.last_location);
+                // const distanceOfDonorFromPreferedLocation = calculateDistance(req.user.last_location, obj.last_location);
+                notification_tokens.push(obj.notification_token);
                 return { fullname, age, weight, distanceFromPreferedLocation, distanceFromuserLocation, donorId:_id };
               }).sort((a,b)=>b.distanceFromPreferedLocation - a.distanceFromPreferedLocation);
               
-            
             res.json({donors}).status(201)
+            await sendNotification(notification_tokens,`Requested by: ${req.user.fullname}, Emergency: ${emergency}, Location: ${location}`)
         }else{
             res.send("No Donors found at the moment")
         }
@@ -30,11 +34,13 @@ const saveBloodRequest = asyncHandler(async (req, res) => {
             console.log(gender);
             const _donors = await User.find({'profile.blood_group':blood_group,'_id': { $ne: initiator },"profile.gender":gender});
             if(_donors.length>0){
-                const donors = _donors.map((obj) => {
+                const donors = _donors.map(async(obj) => {
                     const { fullname, profile,_id } = obj;
                     const { age, weight } = profile[0];
                     const distanceFromuserLocation = calculateDistance(cordinates, req.user.last_location);
                     const distanceFromPreferedLocation = calculateDistance(cordinates, obj.last_location);
+                    const distanceOfDonorFromPreferedLocation = calculateDistance(req.user.last_location, obj.last_location);
+                await sendNotification(obj.notification_token,`Requested by: ${req.user.fullname}, Emergency: ${emergency}, Location: ${location}, Distance: ${distanceOfDonorFromPreferedLocation}`)
                     return { fullname, age, weight, distanceFromPreferedLocation, distanceFromuserLocation, donorId:_id };
                   }).sort((a,b)=>b.distanceFromPreferedLocation - a.distanceFromPreferedLocation);
                   
---
+++ utils/sendNotification.js
@@ -0,0 +1,22 @@
+const { default: axios } = require("axios")
+
+module.exports.sendNotification = async (userToken,message) => {
+    axios.post("https://fcm.googleapis.com/fcm/send",
+        {
+            "registration_ids": userToken,
+            "collapse_key": "New Blood Request",
+            "priority": "high",
+            "notification": {
+                "title": "Blood Request",
+                "body": message
+            }
+        },{
+            headers:{
+                Authorization:"key=AAAAsHV4IqU:APA91bFx4b5z3UXuUd9lrsNNqC9YEl5J61FRkbqYbkXEOQmnrnYF25TX-kAbRyHGWa2UjZOg8NNRB0A3mZZF7yjfZeCWMVLhyJ60Wb_VV9aJoyiOLxBo_uiy2f2UmDxCeZfL8uovBIsi",
+                "Content-Type":"application/json"
+            }
+        }
+    ).then(res=>{
+        console.log(res.data);
+    }).catch(er=>console.log(er))
+}
