9c038219d584e40b513043acf39037c41f54d446
sudeepmi <sudeepm0599@gmail.com>
2024-04-07 12:52:28 +0545

    ..4

--- routes/infoRouter.js
+++ routes/infoRouter.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 
 const { protect, admin } = require("../middlewares/authMiddleware");
-const { getAllinfo, getUsersByBloodGroup } = require("../controllers/infoController");
+const { getAllinfo, getUsersByBloodGroup, getBloodDonationData } = require("../controllers/infoController");
 
 
 router.get("/all", getAllinfo);
 router.get("/bg-users", getUsersByBloodGroup)
+router.get("/graph", getBloodDonationData)
+
 
 
 
